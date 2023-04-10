281a72b485455ce4c5632a10cc558268fd95ff48
Ilaha1 <126712789+Ilaha1@users.noreply.github.com>
2023-04-10 00:58:02 -0700

    Add files via upload

---
+++ 18.Object/script.js
@@ -0,0 +1,26 @@
+// 8
+const obj1={
+    id:1,
+    firstName:'Ilahe',
+    lastName: 'Guliyeva',
+    location:'GDU 2',
+}
+// 9
+const obj2={
+    id:1,
+    firstName:'Ilahe',
+    lastName: 'Guliyeva',
+    location:'GDU 2',
+}
+console.log ('obj2',obj1);
+
+// 13
+const obj3={
+    id:1,
+    firstName:'Ilahe',
+    lastName: 'Guliyeva',
+    location:'GDU 2',
+
+}
+delete(obj3.location)
+console.log(obj3);
