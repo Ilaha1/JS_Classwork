38e60fed8999f805a4c765fea12884ce25c9e4a7
Ilaha1 <126712789+Ilaha1@users.noreply.github.com>
2023-05-04 22:15:44 -0700

    Add files via upload

---
+++ 16.Storage/script.js
@@ -0,0 +1,71 @@
+let btn=document.querySelector(".btn-add");
+
+// 1
+// btn.addEventListener("click",function(){
+//     localStorage.setItem("firstname","Kamran");
+//     localStorage.setItem("lastname","Akberov");
+//     localStorage.setItem("age","23");
+// });
+
+
+// 2
+// for(let i=0; i<localStorage.length; i++) {
+//     let value = localStorage.key(i);
+//     console.log((`${value}`));
+//   }
+
+
+//   3
+
+  
+// for (let i = 0; i < localStorage.length; i++) {
+//     console.log(localStorage.getItem(localStorage.key(i)));
+//   };
+
+// 4
+// let value = localStorage.key(0);
+//     console.log(value);
+
+
+//   4.1
+// let arr1=[];
+// for(let i=0; i<localStorage.length; i++) {
+    
+//    arr1.push(localStorage.key(i));
+//   }
+// console.log(arr1);
+
+
+// // 5
+// let arr=[];
+// for(let i=0; i<localStorage.length; i++) {
+    
+//    arr.push(localStorage.value(i));
+//   }
+// console.log(arr);
+
+
+
+// 5.1
+// let arr=[];
+// for(let i=0; i<localStorage.length; i++) {
+//     let key = localStorage.key(i);
+//    const key1= localStorage.getItem(key);
+//    arr.push(localStorage.getItem(key));
+//   }
+// console.log(arr);
+
+
+// 6
+//  let deleteBtn=document.querySelector(".delete");
+//  let emptyBtn=document.querySelector(".empty");
+
+//  deleteBtn.addEventListener("click",function(){
+// localStorage.removeItem("firstname")
+//  });
+//  6.1
+//  emptyBtn.addEventListener("click",function(){
+// localStorage.clear("")
+//  });
+
+
