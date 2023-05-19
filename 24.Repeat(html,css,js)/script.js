1f181b6f1a05d3c7afd9c7c92ef1b4fcabd8fc3a
Ilahe Quliyeva <756ejv6@code.edu.az>
2023-05-19 14:14:55 +0400

    repeat(html,css,js)

---
+++ 24.Repeat(html,css,js)/script.js
@@ -0,0 +1,92 @@
+const BASE_URL = 'http://localhost:8080/users'
+let isAvailable
+const tBody = document.querySelector('#tBody')
+const burgerModal = document.querySelector('.burger-modal')
+const burgerBar = document.querySelector('.burger-bar')
+const modalRemove = document.querySelector('#modal-remove')
+
+
+async function getAllUsers() {
+    const res = await axios.get(BASE_URL)
+    const data = await res.data
+    tBody.innerHTML = ''
+    data.forEach((user) => {
+        const tr = document.createElement('tr')
+        tr.innerHTML = `
+        <div class='card' style='padding:20px'>
+        <div class='title'>
+        <span style='color:blue'>Id: ${user.id}</span>
+        <span style='color:green; margin-left:10px'>Date: ${user.date}</span>
+        </div>
+        <br>
+        <div class='content d-flex'>
+        <img alt='photo' src='${user.photo}' width="40" height="40"  style="border-radius: 100%; margin-top:3px" alt="">
+        <div style='margin-left:20px'>
+        <span >${user.name} ${user.surname}</span>
+        <br>
+        <span>${user.email}</span>
+        <span class='actions' style='margin-left:300px'>
+        <a href="#" onclick=editUser('${user.id}') class="btn btn-success btn-sm">Edit</a>
+        <a href="#" onclick=deleteUser('${user.id}') class="btn btn-danger btn-sm">Delete</a>
+        <a href="#" onclick=addFavUser('${user.id}') class="btn btn-primary btn-sm">Add fav</a>
+        </span>
+        </div>
+        </div>
+        </div>
+
+        <td>${user.id}</td>
+        <td><img src='${user.photo}' width="40" height="40" style="border-radius: 100%;" alt=""></td>
+        <td>${user.name}</td>
+        <td>${user.surname}</td>
+        <td>${user.email}</td>
+        <td>${user.date}</td>
+        <td>
+        <a href="#" onclick=editUser('${user.id}') class="btn btn-success btn-sm">Edit</a>
+        <a href="#" onclick=deleteUser('${user.id}') class="btn btn-danger btn-sm">Delete</a>
+        <a href="#" onclick=addFavUser('${user.id}') class="btn btn-primary btn-sm btn-fav">Add Fav</a>
+        </td> 
+        `
+        tBody.append(tr)
+    })
+}
+getAllUsers()
+
+
+
+async function deleteUser(userId) {
+    await axios.delete(`${BASE_URL}/${userId}`)
+}
+
+
+function editUser(userId) {
+    window.location.href = `userForm.html?id=${userId}`
+}
+
+
+burgerBar.addEventListener('click', () => {
+    burgerModal.style.display = 'block'
+})
+
+
+
+modalRemove.addEventListener('click', () => {
+    burgerModal.style.display = 'none'
+})
+
+
+const favUsers = JSON.parse(localStorage.getItem('fav-users')) || []
+
+async function addFavUser(userId) {
+    const btnFav = document.querySelector('.btn-fav')
+    const res = await axios.get(`${BASE_URL}/${userId}`)
+    const data = await res.data
+    isAvailable = favUsers.some((user) => user.id === data.id)
+    if (!isAvailable) {
+        favUsers.push(data)
+        localStorage.setItem('fav-users', JSON.stringify(favUsers))
+    } else {
+        alert('Character already exists in favorite list!')
+    }
+}
+
+
