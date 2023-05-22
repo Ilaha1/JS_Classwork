1ae230ce5d43a1631d09f52e26f5ad439415cd90
Ilahe Quliyeva <756ejv6@code.edu.az>
2023-05-22 14:04:12 +0400

    drimo

---
+++ Exam preparation/script.js
@@ -0,0 +1,15 @@
+BASE_URL ="http://localhost:8000/users"
+
+
+// async function getUsers() {
+//     const res = await axios.get(BASE_URL)
+//     const data = await res.data
+//     card.innerHTML = ''
+//     data.forEach((user) => {
+     
+//         card.innerHTML = `
+    
+
+//         `
+//     });
+// }
